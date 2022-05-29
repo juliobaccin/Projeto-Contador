cf76ec3ad7d2abbe5275011adc750a1bd1c7fbb5
Julio Cesar Baccin <juliobassin@hotmail.com>
2022-05-29 01:45:45 -0300

    confirmar

--- script.js
+++ script.js
@@ -12,6 +12,11 @@ function contar() {
         let i = Number(ini.value)
         let f = Number(fim.value)
         let p = Number(passo.value)
+        if (p <= 0) {
+            window.alert('[ERRO] Numero de passo invalido, reconsiderando passo para 1 ')
+            p = 1
+
+        }
         if (i < f) {
             //contagem crescente
             for (let c = i; c <= f; c += p) {
